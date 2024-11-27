4cb5cc4c6671d4c73873af55490c6c87e81a9ae9
ye8n <skyeon7@gmail.com>
2024-11-27 17:59:17 +0900

    20241127 백업

--- project2/project2.js
+++ project2/project2.js
@@ -23,31 +23,32 @@ $("#menuToggle").click(function() {
     }
 });
 
-
-
+/* 
 // PC 메뉴에서 마우스 올리면 서브 메뉴 보이기/숨기기
 document.querySelectorAll('.menu-item').forEach(menuItem => {
-    menuItem.addEventListener('mouseenter', () => {
-        // 서브 메뉴를 보여주기
-        const subMenu = menuItem.querySelector('.sub-global-navigation-bar');
-        if (subMenu) {
-            subMenu.style.display = 'block';
-        } 
-    });
-    // 서브 메뉴 항목 클릭 시 해당 메뉴가 클릭된 상태 유지
-    const subMenu = menuItem.querySelector('.sub-global-navigation-bar');
-    if (subMenu) {
-        subMenu.addEventListener('mouseenter', () => {
-            subMenu.style.display = 'block'; // 마우스를 서브 메뉴에 올렸을 때 보이게 유지
-        });
-        // subMenu.addEventListener('mouseleave', () => {
-        //     subMenu.style.display = 'none'; // 마우스를 서브 메뉴에서 내리면 사라지게
-        // });
-    }
+  menuItem.addEventListener('mouseenter', () => {
+      // 서브 메뉴를 보여주기
+      const subMenu = menuItem.querySelector('.sub-global-navigation-bar');
+      if (subMenu) {
+          subMenu.style.display = 'block';
+      } 
+  });
+  // 서브 메뉴 항목 클릭 시 해당 메뉴가 클릭된 상태 유지
+  const subMenu = menuItem.querySelector('.sub-global-navigation-bar');
+  if (subMenu) {
+      subMenu.addEventListener('mouseenter', () => {
+          subMenu.style.display = 'block'; // 마우스를 서브 메뉴에 올렸을 때 보이게 유지
+      });
+      // subMenu.addEventListener('mouseleave', () => {
+      //     subMenu.style.display = 'none'; // 마우스를 서브 메뉴에서 내리면 사라지게
+      // });
+  }
 });
+ */
+
 
 const mainSwiper = new Swiper("#main-slide", {
-  speed: 600,
+  speed: 900,
   parallax: true,// 시간차 움직임
   pagination: {
     el: ".swiper-pagination",
