ba134869c3b7989762ee8a717f4fec201669a19c
ye8n <skyeon7@gmail.com>
2024-12-16 00:53:52 +0900

    20241215 marquee

--- project3/project3.js
+++ project3/project3.js
@@ -2,7 +2,7 @@ const swiper = new Swiper(".swiper-slider", {
   // Optional parameters
   centeredSlides: true,
   slidesPerView: 1,
-  grabCursor: true,
+  grabCursor: false,
   freeMode: false,
   loop: true,
   mousewheel: false,
@@ -83,3 +83,75 @@ let smoother = ScrollSmoother.create({
 });
 gsap.config({trialWarn: false});
 
+// 
+// GSAP 및 ScrollTrigger 등록
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.set(".intro-bottom h1", { x: "18vw" }); 
+
+gsap.to(".intro-bottom h1", {
+  x: "-35vw", // 텍스트를 화면 왼쪽으로 이동 (조정 가능)
+  ease: "none", // 일정한 속도로 이동
+  scrollTrigger: {
+    trigger: ".intro-bottom", // 트리거할 요소
+    start: "top 100%", // 화면 상단과 요소 상단이 만날 때 시작
+    end: "bottom top", // 100vh 스크롤 종료 지점
+    scrub: 1, // 스크롤 진행에 따라 부드럽게 애니메이션
+  },
+});
+
+// 
+
+
+const lines = [
+  { selector: '.line1 .text', duration: 8 },
+  { selector: '.line2 .text', duration: 10 },
+  { selector: '.line3 .text', duration: 6 }
+];
+
+// 반복되는 부분을 함수로 처리
+lines.forEach(line => {
+  const textElement = document.querySelector(line.selector);
+  const textWidth = textElement.offsetWidth;
+
+  gsap.fromTo(textElement,
+    { x: -textWidth / 2 },
+    {
+      x: 0,
+      duration: line.duration,
+      repeat: -1,
+      ease: 'linear'
+    }
+  );
+});
+
+// 
+gsap.to(".event-icon", {
+  rotation: 360, // 2바퀴 회전 (720도)
+  scrollTrigger: {
+    trigger: ".event-top", // 스크롤 트리거
+    start: "top 60%", // 화면의 50%부터 시작
+    end: "bottom top", // 스크롤이 끝날 때 (스크롤 끝에서 2바퀴 회전)
+    scrub: 5, // 스크롤에 따라 애니메이션 진행, 숫자를 키우면 더 부드럽고 느리게 회전
+    ease: "linear", // 일정한 속도로 회전
+  }
+});
+
+// 
+
+const marquee = gsap.to(".marquee-s-container", {
+  x: "-100%", // 오른쪽에서 왼쪽으로 이동
+  duration: 15, // 15초 동안 이동
+  repeat: -1, // 무한 반복
+  ease: "linear", // 일정한 속도로 이동
+});
+
+// 마우스를 올리면 애니메이션 일시정지
+document.querySelector('.marquee-s').addEventListener('mouseenter', () => {
+  marquee.pause(); // 애니메이션 일시정지
+});
+
+// 마우스를 떼면 애니메이션 다시 시작
+document.querySelector('.marquee-s').addEventListener('mouseleave', () => {
+  marquee.resume(); // 애니메이션 재개
+});
